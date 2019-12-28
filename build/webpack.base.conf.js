af7d3b54efd21eee146acbeb9268347addd7de76
bzhu <zhubicen@gmail.com>
2019-12-28 22:30:35 +0800

    add build progress

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,10 +5,21 @@ var config = require('../config')
 var VueLoaderPlugin = require('vue-loader/lib/plugin')
 var vueLoaderConfig = require('./vue-loader.conf')
 var StylelintPlugin = require('stylelint-webpack-plugin')
+const cliProgress = require('cli-progress');
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+bar1.start(100, 0);
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
+const handler = (percentage, message, ...args) => {
+  // e.g. Output each progress message directly to the console:
+  bar1.update(percentage * 100);
+  if (percentage * 100 >= 100) {
+    bar1.stop();
+  }
+
+};
 
 module.exports = {
   entry: {
@@ -68,6 +79,10 @@ module.exports = {
           resolve('test'),
           resolve('node_modules/mermaid')
         ],
+
+        options: {
+          cacheDirectory: true
+        },
         exclude: [
           resolve('node_modules/mermaid/src/diagrams/class/parser'),
           resolve('node_modules/mermaid/src/diagrams/flowchart/parser'),
@@ -104,6 +119,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       VERSION: JSON.stringify(require('../package.json').version)
-    })
+    }),
+    new webpack.ProgressPlugin(handler)
   ]
 }
